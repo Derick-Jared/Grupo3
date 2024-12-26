769b36a6efd36ca04be9eabf2d28c37be335fae8
Derick-Jared <108320862+Derick-Jared@users.noreply.github.com>
2024-12-26 18:54:33 -0500

    feat: agregar crud de Empleado

---
+++ Usuarios/src/main/java/TDA2024/Usuarios/routes/EmpleadoRoutes.java
@@ -0,0 +1,10 @@
+package TDA2024.Usuarios.routes;
+
+public class EmpleadoRoutes {
+    public static final String BASE = "empleado"; 
+    public static final String GET_ALL ="/all";  
+    public static final String GET_BY_ID = "/get/{id}"; 
+    public static final String CREATE = "/create"; 
+    public static final String UPDATE = "/update"; 
+    public static final String DELETE =  "/delete/{id}";  
+}

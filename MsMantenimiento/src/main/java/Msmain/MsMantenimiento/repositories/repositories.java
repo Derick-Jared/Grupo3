c26783949289b494beb2eda1a3786edcb278cbe2
edrega <edwharregaed@gmail.com>
2025-01-07 16:10:49 -0500

    Repositories

--- MsMantenimiento/src/main/java/Msmain/MsMantenimiento/repositories/repositories.java
+++ MsMantenimiento/src/main/java/Msmain/MsMantenimiento/repositories/repositories.java
@@ -1,3 +1,12 @@
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 public class repositories {
-    
+   @Repository
+public interface IDetalleEnvioRepository extends CrudRepository<DetalleEnvioModel,Integer>{
+
+    @Query("SELECT c from DetalleEnvioModel c where c.estado != 'ELIM'")
+    public List<DetalleEnvioModel> findAllHab();
+} 
 }

85ee84a1b1d3509c0140d30d2adc676b4d67fdb6
isa-batch <isaipaucardevices@gmail.com>
2025-01-07 10:07:10 -0500

    feat: creacion estructura front

--- Proyecto Grupo 3/Catalogo/src/test/java/Catalogo/Catalogo/CatalogoApplicationTests.java
+++ Proyecto Grupo 3/Catalogo/src/test/java/Catalogo/Catalogo/CatalogoApplicationTests.java
@@ -1,18 +1,10 @@
-<<<<<<<< HEAD:Proyecto Grupo 3/Catalogo/src/test/java/Catalogo/Catalogo/CatalogoApplicationTests.java
 package Catalogo.Catalogo;
-========
-package envios.envios;
->>>>>>>> feature/Envios:Proyecto Grupo 3/envios/src/test/java/envios/envios/EnviosApplicationTests.java
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-<<<<<<<< HEAD:Proyecto Grupo 3/Catalogo/src/test/java/Catalogo/Catalogo/CatalogoApplicationTests.java
 class CatalogoApplicationTests {
-========
-class EnviosApplicationTests {
->>>>>>>> feature/Envios:Proyecto Grupo 3/envios/src/test/java/envios/envios/EnviosApplicationTests.java
 
 	@Test
 	void contextLoads() {

76accd567c4f80248b068103c842a9dc200f6443
Derick-Jared <108320862+Derick-Jared@users.noreply.github.com>
2025-01-13 05:23:20 -0500

    feat: empleado model

---
+++ FrontTDA/src/app/models/empleado.model.ts
@@ -0,0 +1,10 @@
+export interface Persona{
+    id:number;
+    cargo:string;
+    estado:string;
+    idPersona:number;
+    creado_por?: number;
+    creado_en?: string;
+    actualizado_por?: number;
+    actualizado_en?: string;
+}

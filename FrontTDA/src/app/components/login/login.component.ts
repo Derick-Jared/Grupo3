6cbb0a10d33ef91a0113efb0b8cdd252d13e96ae
Derick-Jared <108320862+Derick-Jared@users.noreply.github.com>
2025-01-13 03:34:52 -0500

    fix: component login y navbar

--- FrontTDA/src/app/components/login/login.component.ts
+++ FrontTDA/src/app/components/login/login.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/models/usuario.model'
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginError: boolean = false;
+  userForm!: FormGroup;
+  user: Usuario | undefined;
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      email: [this.user?.email || '', Validators.required],
+      password: [this.user?.password || '', [Validators.required]]
+    })
+  }
+
+  constructor(private userService: UsuarioService, private fb: FormBuilder, private router: Router) {
+    this.userForm = this.fb.group({
+      email: [''],
+      password: ['']
+    })
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+      this.userService.login(this.userForm.value).subscribe(data => {
+        console.log(data);
+        if (data) {
+          this.router.navigate(['/index']);
+          this.loginError = false;
+        } else {
+          this.loginError = true;  
+          this.router.navigate(['/login']);
+          this.resetForm();
+        }
+      }, error => {
+        console.log(error);
+        this.loginError = true; 
+      });
+    }else{
+      this.loginError = true;
+    }
+  }
+
+
+  resetForm() {
+    this.userForm.reset();
+  }
 
 }
--- FrontTDA/src/app/services/usuario.service.ts
+++ FrontTDA/src/app/services/usuario.service.ts
@@ -9,11 +9,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UsuarioService {
 
-  url= "http://localhost:8086/usuario/all";
+  url= "http://localhost:8086/usuario/";
 
   constructor( private http: HttpClient) { }
 
   getUsuarios(): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>(this.url);
+    return this.http.get<Usuario[]>(this.url+"all");
+  }
+
+  login(user : any): Observable<Usuario>{
+    console.log('como esta entrando :'+user.email+" "+user.password);
+    return this.http.post<Usuario>(this.url+"login",user);
   }
 }
